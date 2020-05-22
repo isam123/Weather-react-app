7689351ad06d2bde764a6756956cb0cdfd1b5f98
Isam123 <ime432@gmail.com>
2020-05-22 05:01:18 -0500

    Updates

---
+++ precache-manifest.1cc0e529819f35ba4903e44bd908d0c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffa53f43052dd15a203ddf03a61961c8",
+    "url": "/Weather-react/index.html"
+  },
+  {
+    "revision": "ad182f780754f39db4e0",
+    "url": "/Weather-react/static/js/2.a662b085.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Weather-react/static/js/2.a662b085.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20b426320c0cd71484a7",
+    "url": "/Weather-react/static/js/main.848eb9b8.chunk.js"
+  },
+  {
+    "revision": "54db37683fe650c84543",
+    "url": "/Weather-react/static/js/runtime-main.03279baf.js"
+  }
+]);
